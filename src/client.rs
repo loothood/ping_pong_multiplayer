a0015e5654ed32e6fdaebebd19da09f6e716e7c3
MacBook <macbook@MacBooks-MacBook-Pro.local>
2021-10-29 21:07:04 +0300

    [*] article 2 	new file:   resources/DejaVuSansMono.ttf 	modified:   src/client.rs 	modified:   src/proto/game.proto 	modified:   src/server.rs

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,11 @@
 use tetra::graphics::{self, Color, Texture};
+use tetra::input::{self, Key};
 use tetra::math::Vec2;
 use tetra::{TetraError};
 use tetra::{Context, ContextBuilder, State};
 use generated_shared::game_proto_client::GameProtoClient;
-use generated_shared::{FloatTuple, PlayGameRequest, PlayGameResponse};
+use generated_shared::{PlayGameRequest, PlayGameResponse, FloatTuple, WorldStatus, ClientActions};
+use tetra::graphics::text::{Text, Font};
 
 mod generated_shared;
 
@@ -17,74 +19,123 @@ async fn establish_connection() -> GameProtoClient<tonic::transport::Channel> {
 fn main() -> Result<(), TetraError> {
     let rt = tokio::runtime::Runtime::new().expect("Error runtime creation");
     let mut client = rt.block_on(establish_connection());
+
     ContextBuilder::new("Pong", WINDOW_WIDTH as i32, WINDOW_HEIGHT as i32)
         .quit_on_escape(true)
         .build()?
-        .run(|ctx|GameState::new(ctx, &mut client))
+        .run(|ctx| GameState::new(ctx, &mut client))
 }
+
+impl From<FloatTuple> for Vec2<f32> {
+    fn from(data: FloatTuple) -> Self {
+        Vec2 {
+            x: data.x,
+            y: data.y,
+        }
+    }
+}
+
 struct Entity {
     texture: Texture,
     position: Vec2<f32>,
     velocity: Vec2<f32>,
 }
+
 impl Entity {
     fn new(texture: &Texture, position: Vec2<f32>) -> Entity {
         Entity::with_velocity(&texture, position, Vec2::zero())
     }
+
     fn with_velocity(texture: &Texture, position: Vec2<f32>, velocity: Vec2<f32>) -> Entity {
         Entity { texture: texture.clone(), position, velocity }
     }
 }
+
 struct GameState {
     player1: Entity,
     player2: Entity,
     ball: Entity,
     player_number: u32,
     players_count: u32,
+    winner: u32,
     client: GameProtoClient<tonic::transport::Channel>,
 }
+
 impl GameState {
-        fn new(ctx: &mut Context, client : &mut GameProtoClient<tonic::transport::Channel>) -> tetra::Result<GameState> {
-            let player1_texture = Texture::new(ctx, "./resources/player1.png")?;
-            let ball_texture = Texture::new(ctx, "./resources/ball.png")?;
-            let player2_texture = Texture::new(ctx, "./resources/player2.png")?;
-            let play_request = GameState::play_request(&player1_texture, &player2_texture, &ball_texture, client);
-            let ball = play_request.ball.expect("Cannot get ball's data from server");
-            let ball_position = ball.position.expect("Cannot get ball position from server");
-            let ball_position = Vec2::new(
-                ball_position.x,
-                ball_position.y,
-            );
-            let ball_velocity = ball.velocity.expect("Cannot get ball velocity from server");
-            let ball_velocity = Vec2::new(
-                ball_velocity.x,
-                ball_velocity.y,
-            );
-            let player1_position = &play_request.player1_position
+    fn new(ctx: &mut Context, client: &mut GameProtoClient<tonic::transport::Channel>)
+           -> tetra::Result<GameState> {
+        let player1_texture = Texture::new(ctx, "./resources/player1.png")?;
+        let ball_texture = Texture::new(ctx, "./resources/ball.png")?;
+        let player2_texture = Texture::new(ctx, "./resources/player2.png")?;
+        let play_request =
+            GameState::play_request(&player1_texture, &player2_texture, &ball_texture, client);
+
+        let ball = play_request.ball.unwrap();
+        let ball_position = Vec2::from(ball.position.unwrap());
+        let ball_velocity = Vec2::from(ball.velocity.unwrap());
+
+        let player1_position: FloatTuple = play_request.player1_position
+            .expect("Cannot get player position from server");
+        let player1_position = Vec2::from(player1_position);
+
+        let player2_position: FloatTuple = play_request.player2_position
+            .expect("Cannot get player position from server");
+        let player2_position = Vec2::from(player2_position);
+
+        let player_number = play_request.current_player_number;
+
+        Ok(GameState {
+            player1: Entity::new(&player1_texture, player1_position),
+            player2: Entity::new(&player2_texture, player2_position),
+            ball: Entity::with_velocity(&ball_texture, ball_position, ball_velocity),
+            player_number,
+            players_count: player_number,
+            // No winner by default
+            winner: 2,
+            client: client.clone(),
+        })
+    }
+
+    fn set_updated_values(&mut self, response: WorldStatus) {
+        let players_count = response.players_count;
+        self.players_count = players_count;
+        if players_count >= 2 {
+            let ball = response.ball.unwrap();
+            let ball_position = Vec2::from(ball.position.unwrap());
+            let ball_velocity = Vec2::from(ball.velocity.unwrap());
+
+            let player1_position: FloatTuple = response.player1_position
                 .expect("Cannot get player position from server");
-            let player1_position = Vec2::new(
-                player1_position.x,
-                player1_position.y,
-            );
-            let player2_position = &play_request.player2_position
+            let player1_position = Vec2::from(player1_position);
+
+            let player2_position: FloatTuple = response.player2_position
                 .expect("Cannot get player position from server");
-            let player2_position = Vec2::new(
-                player2_position.x,
-                player2_position.y,
-            );
-            let player_number = play_request.current_player_number;
-            Ok(GameState {
-                player1: Entity::new(&player1_texture, player1_position),
-                player2: Entity::new(&player2_texture, player2_position),
-                ball: Entity::with_velocity(&ball_texture, ball_position, ball_velocity),
-                player_number,
-                players_count: player_number,
-                client: client.clone(),
-            })
+            let player2_position = Vec2::from(player2_position);
+
+            let winner = &response.winner;
+
+            self.winner = winner.clone() as u32;
+            self.ball.position = ball_position;
+            self.ball.velocity = ball_velocity;
+            self.player1.position = player1_position;
+            self.player2.position = player2_position;
         }
+    }
+
+    #[tokio::main]
+    async fn world_update_request(&self, clicked_button_number: u32, player_number: u32) -> WorldStatus {
+        let request = tonic::Request::new(ClientActions {
+            player_number,
+            clicked_button: clicked_button_number,
+        });
+        let mut client = self.client.clone();
+        client.world_update_request(request)
+            .await.expect("Cannot get World Update from the server").into_inner()
+    }
+
     #[tokio::main]
     async fn play_request(player1_texture: &Texture, player2_texture: &Texture, ball_texture: &Texture,
-                          client : &mut GameProtoClient<tonic::transport::Channel>) -> PlayGameResponse {
+                          client: &mut GameProtoClient<tonic::transport::Channel>) -> PlayGameResponse {
         let request = tonic::Request::new(PlayGameRequest {
             window_size: Some(FloatTuple { x: WINDOW_WIDTH, y: WINDOW_HEIGHT }),
             player1_texture: Some(
@@ -100,16 +151,47 @@ impl GameState {
         client.play_request(request).await.expect("Cannot get Play Response the server").into_inner()
     }
 }
+
 impl State for GameState {
     fn update(&mut self, ctx: &mut Context) -> tetra::Result {
+        let mut clicked_button = 2;
+
+        if input::is_key_down(ctx, Key::Up) {
+            clicked_button = 0;
+        }
+        if input::is_key_down(ctx, Key::Down) {
+            clicked_button = 1;
+        }
+
+        let world_update_request =
+            self.world_update_request(clicked_button, self.player_number);
+        self.set_updated_values(world_update_request);
 
         Ok(())
     }
+
     fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
         graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
-        self.player1.texture.draw(ctx, self.player1.position);
-        self.player2.texture.draw(ctx, self.player2.position);
-        self.ball.texture.draw(ctx, self.ball.position);
+        // 0 - Player 1 won
+        // 1 - Player 2 won
+        if self.winner == 2 {
+            self.player1.texture.draw(ctx, self.player1.position);
+            self.ball.texture.draw(ctx, self.ball.position);
+            self.player2.texture.draw(ctx, self.player2.position);
+        } else {
+            let text_offset: Vec2<f32> = Vec2::new(16.0, 16.0);
+            let mut message = format!("Winner is: Player ");
+            if self.winner == 0 {
+                message += "1";
+            } else {
+                message += "2";
+            }
+            let mut t: Text = Text::new(message,
+                                        Font::vector(ctx, "./resources/DejaVuSansMono.ttf",
+                                                     16.0)?,
+            );
+            t.draw(ctx, text_offset);
+        }
         Ok(())
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,26 @@
 use tonic::{transport::Server, Response};
 use generated_shared::game_proto_server::{GameProto, GameProtoServer};
-use generated_shared::{Ball, FloatTuple, PlayGameRequest, PlayGameResponse};
+use generated_shared::{Ball, ClientActions, FloatTuple, PlayGameRequest, PlayGameResponse, WorldStatus};
 use std::sync::{Mutex, Arc};
 use tetra::math::Vec2;
+use tetra::graphics::Rectangle;
 use rand::Rng;
 
 mod generated_shared;
 
 const BALL_SPEED: f32 = 5.0;
+const PADDLE_SPEED: f32 = 8.0;
+const PADDLE_SPIN: f32 = 4.0;
+const BALL_ACC: f32 = 0.05;
+
+impl From<Vec2<f32>> for FloatTuple  {
+    fn from(data: Vec2<f32>) -> Self {
+        FloatTuple {
+            x: data.x,
+            y: data.y,
+        }
+    }
+}
 
 #[derive(Clone)]
 struct Entity {
@@ -15,14 +28,41 @@ struct Entity {
     position: Vec2<f32>,
     velocity: Vec2<f32>,
 }
+
 impl Entity {
     fn new(texture_size: Vec2<f32>, position: Vec2<f32>) -> Entity {
         Entity::with_velocity(texture_size, position, Vec2::zero())
     }
+
     fn with_velocity(texture_size: Vec2<f32>, position: Vec2<f32>, velocity: Vec2<f32>) -> Entity {
         Entity { texture_size, position, velocity }
     }
+
+    fn width(&self) -> f32 {
+        self.texture_size.x
+    }
+
+    fn height(&self) -> f32 {
+        self.texture_size.y
+    }
+
+    fn centre(&self) -> Vec2<f32> {
+        Vec2::new(
+            self.position.x + (self.width() / 2.0),
+            self.position.y + (self.height() / 2.0),
+        )
+    }
+
+    fn bounds(&self) -> Rectangle {
+        Rectangle::new(
+            self.position.x,
+            self.position.y,
+            self.width(),
+            self.height(),
+        )
+    }
 }
+
 #[derive(Clone)]
 struct World {
     player1: Entity,
@@ -31,10 +71,12 @@ struct World {
     world_size: Vec2<f32>,
     winner: u32,
 }
+
 pub struct PlayGame {
     world: Arc<Mutex<Option<World>>>,
     players_count: Arc<Mutex<u32>>,
 }
+
 impl PlayGame {
     fn new() -> PlayGame {
         PlayGame {
@@ -42,23 +84,29 @@ impl PlayGame {
             players_count: Arc::new(Mutex::new(0u32)),
         }
     }
+
     fn init(&self, window_size: FloatTuple, player1_texture: FloatTuple,
             player2_texture: FloatTuple, ball_texture: FloatTuple) {
         let window_width = window_size.x;
         let window_height = window_size.y;
+
         let world = Arc::clone(&self.world);
         let mut world = world.lock().unwrap();
         let players_count = Arc::clone(&self.players_count);
         let players_count = players_count.lock().unwrap().clone();
-        let mut ball_velocity = 0f32;
-        if players_count >= 2 {
-            let num = rand::thread_rng().gen_range(0..2);
-            if num == 0 {
-                ball_velocity = -BALL_SPEED;
+
+        let ball_velocity = {
+            if players_count < 2 {
+                0f32
             } else {
-                ball_velocity = BALL_SPEED;
+                if rand::thread_rng().gen_range(0..2) == 0 {
+                    -BALL_SPEED
+                } else {
+                    BALL_SPEED
+                }
             }
-        }
+        };
+
         *world =
             Option::Some(World {
                 player1: Entity::new(
@@ -87,16 +135,80 @@ impl PlayGame {
                     ),
                 ),
                 world_size: Vec2::new(window_size.x, window_size.y),
-                // No one win yet
+                // Noone win yet
                 winner: 2,
             });
     }
+
     fn increase_players_count(&self) {
         let players_count = Arc::clone(&self.players_count);
         let mut players_count = players_count.lock().unwrap();
         *players_count += 1;
     }
+
+    fn apply_new_world(&self, new_world: &World) {
+        let world = Arc::clone(&self.world);
+        let mut world = world.lock().unwrap();
+        *world = Option::from(new_world.clone());
+    }
+
+    fn update_world(world: &mut World, clicked_button: u32, player_number: u32) {
+        // 0 - UP
+        // 1 - Down
+        if clicked_button == 0 {
+            if player_number == 1 {
+                world.player1.position.y -= PADDLE_SPEED;
+            } else if player_number == 2 {
+                world.player2.position.y -= PADDLE_SPEED;
+            }
+        } else if clicked_button == 1 {
+            if player_number == 1 {
+                world.player1.position.y += PADDLE_SPEED;
+            } else if player_number == 2 {
+                world.player2.position.y += PADDLE_SPEED;
+            }
+        }
+        world.ball.position += world.ball.velocity;
+
+        let player1_bounds = world.player1.bounds();
+        let player2_bounds = world.player2.bounds();
+        let ball_bounds = world.ball.bounds();
+
+        let paddle_hit = if ball_bounds.intersects(&player1_bounds) {
+            Some(&world.player1)
+        } else if ball_bounds.intersects(&player2_bounds) {
+            Some(&world.player2)
+        } else {
+            None
+        };
+
+        if let Some(paddle) = paddle_hit {
+            world.ball.velocity.x =
+                -(world.ball.velocity.x + (BALL_ACC * world.ball.velocity.x.signum()));
+
+            let offset = (paddle.centre().y - world.ball.centre().y) / paddle.height();
+
+            world.ball.velocity.y += PADDLE_SPIN * -offset;
+        }
+
+        if world.ball.position.y <= 0.0
+            || world.ball.position.y + world.ball.height() >= world.world_size.y
+        {
+            world.ball.velocity.y = -world.ball.velocity.y;
+        }
+
+        if world.ball.position.x < 0.0 {
+            //Player 2 win
+            world.winner = 1;
+        }
+
+        if world.ball.position.x > world.world_size.x {
+            // Player 1 win
+            world.winner = 0;
+        }
+    }
 }
+
 #[tonic::async_trait]
 impl GameProto for PlayGame {
     async fn play_request(
@@ -115,38 +227,62 @@ impl GameProto for PlayGame {
         let current_players = Arc::clone(&self.players_count);
         let current_players = current_players.lock().unwrap();
         let reply = PlayGameResponse {
-            player1_position: Option::Some(FloatTuple {
-                x: world.player1.position.x,
-                y: world.player1.position.y,
-            }),
-            player2_position: Option::Some(FloatTuple {
-                x: world.player2.position.x,
-                y: world.player2.position.y,
-            }),
+            player1_position: Some(FloatTuple::from(world.player1.position)),
+            player2_position: Some(FloatTuple::from(world.player2.position)),
             current_player_number: current_players.clone(),
             players_count: current_players.clone(),
-            ball: Option::Some(Ball {
-                position: Option::Some(FloatTuple {
-                    x: world.ball.position.x,
-                    y: world.ball.position.y,
-                }),
-                velocity: Option::Some(FloatTuple {
-                    x: world.ball.velocity.x,
-                    y: world.ball.velocity.y,
-                }),
+            ball: Some(Ball {
+                position: Some(FloatTuple::from(world.ball.position)),
+                velocity: Some(FloatTuple::from(world.ball.velocity)),
             }),
         };
         Ok(Response::new(reply))
     }
+
+    async fn world_update_request(
+        &self,
+        request: tonic::Request<ClientActions>,
+    ) -> Result<tonic::Response<WorldStatus>, tonic::Status> {
+        let client_actions: ClientActions = request.into_inner();
+        let clicked_button = client_actions.clicked_button;
+        let player_number = client_actions.player_number;
+
+        let players_count = Arc::clone(&self.players_count);
+        let players_count = players_count.lock().unwrap().clone();
+
+        let mut world = Arc::clone(&self.world).lock().unwrap().as_ref().unwrap().clone();
+        if players_count >= 2 {
+            PlayGame::update_world(&mut world, clicked_button, player_number);
+        }
+        self.apply_new_world(&world);
+
+        let players_count = Arc::clone(&self.players_count);
+        let players_count = players_count.lock().unwrap();
+
+        let reply = WorldStatus {
+            player1_position: Some(FloatTuple::from(world.player1.position)),
+            player2_position: Some(FloatTuple::from(world.player2.position)),
+            ball: Some(Ball {
+                position: Some(FloatTuple::from(world.ball.position)),
+                velocity: Some(FloatTuple::from(world.ball.velocity)),
+            }),
+            players_count: players_count.clone(),
+            winner: world.winner,
+        };
+        Ok(Response::new(reply))
+    }
 }
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
     let play_game = PlayGame::new();
+
     println!("Server listening on {}", addr);
     Server::builder()
         .add_service(GameProtoServer::new(play_game))
         .serve(addr)
         .await?;
+
     Ok(())
 }
